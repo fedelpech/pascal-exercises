d92405715a2c5ac45284d55da8f78634d2903070
Felipe Delpech <felipedelpecht@gmail.com>
2023-05-28 13:05:54 -0300

    doing exercises

---
+++ imprimirPos.pas
@@ -0,0 +1,18 @@
+{ 1. Imprimir la tercera posición de un array con cuatro elementos }
+
+program terceraPos;
+
+type
+    cuatroElementos = array[1..4] of integer;
+
+var
+    arreglo: cuatroElementos;
+    i: Integer;
+    
+begin
+    arreglo[3] := 2003;
+
+    for i := 1 to length(arreglo) do
+        writeln(arreglo[i]);
+    
+end.
